9c29e6de23f9ea12919909aab40867cfd66293f8
aran.hu <1903283729@qq.com>
2018-01-18 15:11:54 +0800

    初步改造完成

--- ConfigModel/CommonComponent.js
+++ ConfigModel/CommonComponent.js
@@ -1,16 +1,41 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { connect } from 'react-redux';
 
-// create a component
 class CommonComponent extends Component {
+
+	constructor(props) {
+		super(props)
+
+	}
+	componentDidMount() {
+		console.log('this.props', this.props)
+		// 可以这样动态改
+		// const {setParams} = this.props.navigation;
+		// setParams({name: 'Lucy'})
+	}
 	render() {
-		console.log('CommonComponent', this.props)
-		return (
-			<View style={styles.container}>
-				<Text>CommonComponent</Text>
-			</View>
-		);
+		// console.log('CommonComponent', this.props)
+		const { navigation } = this.props
+		const { routeName } = navigation.state
+		if (routeName == 'p1') {
+			return (
+				<View style={styles.container}>
+					<Text>CommonComponent</Text>
+					<Button
+						onPress={() => {navigation.navigate('p2')}}
+						title="Go to P2"
+					/>
+				</View>
+			);
+		} else {
+			return (
+				<View style={styles.container}>
+					<Text>CommonComponent</Text>
+				</View>
+			);
+		}
 	}
 }
 
@@ -25,4 +50,9 @@ const styles = StyleSheet.create({
 });
 
 //make this component available to the app
-export default CommonComponent;
+// export default CommonComponent;
+
+const mapStateToProps = state => ({
+	state: state,
+});
+export default connect(mapStateToProps)(CommonComponent);
--- ConfigModel/RootNavigator.js
+++ ConfigModel/RootNavigator.js
@@ -37,137 +37,106 @@ import CommonComponent from './CommonComponent'
 //   }
 // }
 
-
-function getWarpComponent(params){
-  const { componentType } = params
-  if (!params) console.warn('配置页面参数为空')
-  return class WarpComponent extends Component {
-
-    render() {
-      // 这里怎么搞？ 动态的props传进来, 先不考虑redux维护用户信息等乱七八糟的
-			/**
-			 * 在这里把每一页的参数都注入进去么？
-			 * 这里应该传入这个Tabs 中包含多少个子页面吧？诶？柑橘是圈梁信息呢
-			 * 包括页面信息: title
-			 * 页面请求参数信息
-			 * 页面跳转信息
-			 */
-
-      /**
-       * 这里主要传递有几个tab就行了
-       */
-
-      return (
-        <TabNav screenProps={params} />
-      );
-    }
-  }
-  const mapStateToProps = state => ({
-    state: state,
-  });
-  const Page = connect(mapStateToProps)(WarpComponent);
-}
-
-function getComponent() {
-  return class extends Component {
-    render() {
-      console.log('getComponent', this.props)
-      return (
-        <CommonComponent screenProps={this.props.screenProps} />
-      );
-    }
-  }
-}
-const config = {
-  'CommonCmponent': getComponent,
-  'WarpComponent': getWarpComponent
-}
-
-const getPage = (key) => {
-  return config[key]
-}
-
-const RootNavigator = StackNavigator(makeNavConfig(), {
-  initialRouteName: 'p2'
-})
-
-function makeNavConfig() {
-  let config = {
-    'p1': {
-      'screen': 'CommonCmponent',
-    },
-    'p2': {
-      'screen': 'WarpComponent',
-      'screenProps': {
-        'componentType': 'Tabs',
-        'childTabs': {
-          '子页面1': {
-            'screen': 'CommonCmponent',
-          },
-          '子页面2': {
-            'screen': 'CommonCmponent',
-          }
+const _config = {
+  'p1': {
+    'screen': 'CommonComponent',
+  },
+  'p2': {
+    'screen': 'WarpComponent',
+    'screenProps': {
+      'componentType': 'Tabs',
+      'childTabs': {
+        '子页面1': {
+          'screen': 'CommonComponent',
+        },
+        '子页面2': {
+          'screen': 'CommonComponent',
         }
       }
     }
   }
+}
 
+function getWarpComponent(params) {
+  if (!params) console.warn('配置页面参数为空')
+  const { screen, screenProps } = params
+  switch (screen) {
+    case 'CommonComponent':
+      return class extends Component {
+        render() {
+          return (
+            <CommonComponent {...this.props} />
+          );
+        }
+      }
+      break;
+    case 'WarpComponent':
+      return class extends Component {
+        render() {
+          let P = getTabNav(screenProps)
+          return (
+            <P {...this.props} />
+          );
+        }
+      }
+      break;
+    default:
+  }
+}
+
+function getTabNav(params) {
+  console.log('传入到Tab的参数', params)
+  let d = makeNavConfig(params.childTabs)
+  const TabNav = TabNavigator(makeNavConfig(params.childTabs), {
+      tabBarPosition: 'top',
+      animationEnabled: true,
+      swipeEnabled: true,
+      tabBarComponent: TabBarTop,
+      tabBarOptions: {
+        scrollEnabled: true,
+        tabStyle: {
+          width: 200,
+        },
+      }
+    });
+  return class extends Component {
+    render() {
+      return (
+        <TabNav screenProps={this.props.screenProps} />
+      );
+    }
+  }
+}
+
+// function getComponent() {
+//   return class extends Component {
+//     render() {
+//       // console.log('getComponent', this.props)
+//       return (
+//         <CommonComponent screenProps={this.props.screenProps} />
+//       );
+//     }
+//   }
+// }
+
+const makeNavConfig = (config = _config) => {
   let navConfig = {}
   Object.keys(config).map((pName, index) => {
     const screenConfig = config[pName]
-    // console.log('screenConfig.screen', screenConfig.screen)
-    const component = getPage(screenConfig.screen)
-    switch (screenConfig.screen) {
-      case 'CommonCmponent':
-        navConfig[pName] = {
-          screen: component(),
-          navigationOptions: ({ navigation }) => ({
-            title: (navigation.state.params && navigation.state.params.name) || 'Default Title'
-          })
-        }
-        break;
-      case 'WarpComponent':
-        navConfig[pName] = {
-          screen: component(screenConfig.screenProps),
-          navigationOptions: ({ navigation }) => ({
-            title: (navigation.state.params && navigation.state.params.name) || 'Default Title'
-          })
-        }
-        break
-      default:
+    const component = getWarpComponent(screenConfig)
+    navConfig[pName] = {
+      screen: component,
+      navigationOptions: ({ navigation }) => ({
+        title: (navigation.state.params && navigation.state.params.name) || pName
+      })
     }
   })
-  // console.log(navConfig)
   return navConfig
 }
 
-
-
-
-
-
-
-const TabNav = TabNavigator({
-  all: {
-    screen: getComponent(),
-    navigationOptions: ({ navigtation }) => ({
-      title: 'all'
-    })
-  },
-}, {
-    tabBarPosition: 'top',
-    animationEnabled: true,
-    swipeEnabled: true,
-    tabBarComponent: TabBarTop,
-    tabBarOptions: {
-      scrollEnabled: true,
-      tabStyle: {
-        width: 200,
-      },
-    }
-  });
-
-
+const RootNavigator = StackNavigator(makeNavConfig(), {
+  initialRouteName: 'p1'
+})
 
 
 
